221d55d3d990285d63f332041d9efd5b73d563ef
root <Nada.Ghannam19@gmail.com>
2026-05-01 15:55:07 +0000

    Fix double linked list deletion

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -2,45 +2,36 @@
 #include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - deletes the node at index of a dlistint_t list
- * @head: pointer to the head of the list
+ * delete_dnodeint_at_index - deletes a node at a specific index
+ * @h: pointer to the head of the list
  * @index: index of the node to delete
  *
- * Return: 1 if it succeeded, -1 if it failed
+ * Return: 1 on success, -1 on failure
  */
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+int delete_dnodeint_at_index(dlistint_t **h, unsigned int index)
 {
-	dlistint_t *current;
+	dlistint_t *head;
 	unsigned int i;
 
-	if (head == NULL || *head == NULL)
+	if (h == NULL || *h == NULL)
 		return (-1);
 
-	current = *head;
+	head = *h;
 
-	if (index == 0)
-	{
-		*head = current->next;
+	for (i = 0; head != NULL && i < index; i++)
+		head = head->next;
 
-		if (*head != NULL)
-			(*head)->prev = NULL;
-
-		free(current);
-		return (1);
-	}
-
-	for (i = 0; current != NULL && i < index; i++)
-		current = current->next;
-
-	if (current == NULL)
+	if (head == NULL)
 		return (-1);
 
-	if (current->prev != NULL)
-		current->prev->next = current->next;
+	if (head->prev != NULL)
+		head->prev->next = head->next;
+	else
+		*h = head->next;
 
-	if (current->next != NULL)
-		current->next->prev = current->prev;
+	if (head->next != NULL)
+		head->next->prev = head->prev;
 
-	free(current);
+	free(head);
 	return (1);
 }
